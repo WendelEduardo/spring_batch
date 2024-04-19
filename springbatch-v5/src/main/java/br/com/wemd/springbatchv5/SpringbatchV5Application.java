4001fe6abeed87a4cfd0f8e789e0f7ea3514ee14
Wendel Eduardo <71601127+WendelEduardo@users.noreply.github.com>
2024-04-18 22:26:01 -0300

    Add files via upload

---
+++ springbatch-v5/src/main/java/br/com/wemd/springbatchv5/SpringbatchV5Application.java
@@ -0,0 +1,13 @@
+package br.com.wemd.springbatchv5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchV5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchV5Application.class, args);
+	}
+
+}
---
+++ springbatch-v5/src/test/java/br/com/wemd/springbatchv5/SpringbatchV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wemd.springbatchv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchV5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
